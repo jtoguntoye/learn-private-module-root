ff8d77ecd92a08511632c0d425b1af4459a0089d
Joel Oguntoye <tosinjoel3@gmail.com>
2021-11-16 21:47:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
-
+  
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/city-allies/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
+  version = "1.0.1"
 }
